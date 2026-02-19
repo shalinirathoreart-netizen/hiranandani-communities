537170fa99c2aa6fbbf506a15dce66baa67ae6e1
shalinirathoreart-netizen <shalinirathoreart@gmail.com>
2026-02-19 15:04:47 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,15 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("content");
+
+  eleventyConfig.addCollection("blog", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/blog/*.md");
+  });
 
   return {
     dir: {
       input: ".",
+      includes: "_includes",
       output: "_site"
     }
   };
